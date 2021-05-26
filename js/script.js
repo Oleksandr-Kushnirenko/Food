cafb64f29b63abbc111ae2c4d62b1b7c70e3cd36
Kushnirenko Oleksandr <kushnirenko.oleksandr@gmail.com>
2021-05-26 13:34:26 +0300

    Do lesson 65

--- js/script.js
+++ js/script.js
@@ -541,11 +541,15 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    function deleteNotDidits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNotDidits(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDidits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -573,9 +577,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDidits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDidits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -604,7 +608,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDidits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
