68bec412641d146c0b81a9fd2ccd93bc27d0b328
Oleksandr <oleksandr.kushnirenko@gmail.com>
2025-03-29 23:09:06 +0200

    Создание табов. урок 63

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,40 @@
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
-
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParenr = document.querySelector('.tabheader__items');
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      // item.style.display = "none";
+      item.classList.add("hide");
+      item.classList.remove('show', "fade"); // удаляем так же класс анимации
+    });
+    tabs.forEach(item => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+  function showTabContent(i = 0) {
+    // tabsContent[i].style.display = "block";
+    tabsContent[i].classList.add("show", "fade"); // добавляем так же класс анимации
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add("tabheader__item_active");
+  }
+  hideTabContent();
+  showTabContent();
+  tabsParenr.addEventListener("click", event => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
 /******/ })()
 ;
+//# sourceMappingURL=script.js.map
